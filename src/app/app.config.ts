5ca8b6579cadc6a225bf77ebdb332298b7f7a645
Mihael0599 <mihaeldraz10@gmail.com>
2025-06-24 19:34:45 +0200

    installed angular material and firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,10 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "dabubble-cd773", appId: "1:664512726615:web:9a234f4d135c2d86c14512", storageBucket: "dabubble-cd773.firebasestorage.app", apiKey: "AIzaSyB9wuP1tr9i6oUgJ7aQp4SNcqrxeFuIrWs", authDomain: "dabubble-cd773.firebaseapp.com", messagingSenderId: "664512726615" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
 };
