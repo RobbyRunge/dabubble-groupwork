e1b48ba620765be82dd839a78bac90a70b4ca9d1
RobbyR98 <robbyrunge@yahoo.com>
2025-06-25 18:54:03 +0200

    refactor: remove placeholder content and implement Firestore data fetching

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { inject } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+
+interface Item {
+  name: string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'DaBubble';
+
+  firestore = inject(Firestore);
+  itemCollection = collection(this.firestore, 'users');
+  item$ = collectionData(this.itemCollection);
 }
