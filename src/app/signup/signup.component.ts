873ff7cd73409c96da4449f97b88a1cac381cc26
RobbyR98 <robbyrunge@yahoo.com>
2025-06-27 19:55:20 +0200

    style: enhance signup component with back navigation and policy agreement text

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderStartComponent } from "../shared/header-start/header-start.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
-  imports: [HeaderStartComponent],
+  imports: [HeaderStartComponent, RouterLink],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
