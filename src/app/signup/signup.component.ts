458f3f5f0b8cc034fd9a91e4cf65a9af5753d2c5
RobbyR98 <robbyrunge@yahoo.com>
2025-06-27 20:56:33 +0200

    style: implement policy agreement checkbox with hover effects and disable button based on acceptance

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -9,5 +9,18 @@ import { RouterLink } from '@angular/router';
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
+  isPolicyAccepted = false;
+  isHovering = false;
 
+  togglePolicy() {
+    this.isPolicyAccepted = !this.isPolicyAccepted;
+  }
+
+  getCheckboxImage(): string {
+    if (this.isPolicyAccepted) {
+      return this.isHovering ? 'signup/box-checked-hover.png' : 'signup/box-checked.png';
+    } else {
+      return this.isHovering ? 'signup/box-hover.png' : 'signup/box.png';
+    }
+  }
 }
