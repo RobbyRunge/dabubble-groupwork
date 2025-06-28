043a9286b9a7a9eb8a8df1dbfaa5039ed1f36fac
RobbyR98 <robbyrunge@yahoo.com>
2025-06-28 19:41:26 +0200

    style: refactor avatar component structure and styles for improved layout and navigation

--- src/app/signup/avatar/avatar.component.ts
+++ src/app/signup/avatar/avatar.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderStartComponent } from "../../shared/header-start/header-start.component";
+import { FooterStartComponent } from "../../shared/footer-start/footer-start.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-avatar',
-  imports: [],
+  imports: [HeaderStartComponent, FooterStartComponent, RouterLink],
   templateUrl: './avatar.component.html',
   styleUrl: './avatar.component.scss'
 })
