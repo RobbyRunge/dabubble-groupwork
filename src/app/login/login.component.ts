c7abbed265f768d0b5f38c7ada57e7f5bc90f4fb
RobbyR98 <robbyrunge@yahoo.com>
2025-06-28 18:49:22 +0200

    style: implement intro logo display logic in login component and add IntroService for tracking intro visibility

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { FooterStartComponent } from "../shared/footer-start/footer-start.component";
+import { CommonModule } from '@angular/common';
+import { IntroService } from '../services/intro.service';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink, FooterStartComponent],
+  imports: [RouterLink, FooterStartComponent, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  showIntroLogo = false;
 
+  private introService = inject(IntroService);
+
+  ngOnInit() {
+    if (!this.introService.hasIntroBeenShown()) {
+      this.showIntroLogo = true;
+      this.introService.markIntroAsShown();
+      setTimeout(() => {
+        this.showIntroLogo = false;
+      }, 6000);
+    } else {
+      this.showIntroLogo = false;
+    }
+  }
 }
