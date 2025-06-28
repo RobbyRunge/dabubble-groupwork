fe570e3c1d25eac1ee925b3eb37d22e08139c08d
RobbyR98 <robbyrunge@yahoo.com>
2025-06-28 19:07:57 +0200

    style: add footer component to signup page

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderStartComponent } from "../shared/header-start/header-start.component";
 import { RouterLink } from '@angular/router';
+import { FooterStartComponent } from "../shared/footer-start/footer-start.component";
 
 @Component({
   selector: 'app-signup',
-  imports: [HeaderStartComponent, RouterLink],
+  imports: [HeaderStartComponent, RouterLink, FooterStartComponent],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
