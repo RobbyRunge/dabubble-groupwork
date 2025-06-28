808d41fc5ed03a6c9fc4d912743b36169776b3e6
Mihael0599 <mihaeldraz10@gmail.com>
2025-06-28 15:57:43 +0200

    in Workspace added channel image name and edit

--- src/app/main-content/work-space-section/work-space-section.component.ts
+++ src/app/main-content/work-space-section/work-space-section.component.ts
@@ -3,6 +3,15 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatDrawer, MatSidenavModule } from '@angular/material/sidenav';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIcon } from '@angular/material/icon';
+import {
+  FormControl,
+  FormGroupDirective,
+  NgForm,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-work-space-section',
@@ -10,7 +19,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     MatButtonModule,
     MatSidenavModule,
     MatSelectModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatIcon,
   ],
   templateUrl: './work-space-section.component.html',
   styleUrl: './work-space-section.component.scss'
