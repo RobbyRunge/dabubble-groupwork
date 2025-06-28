249f94f399fbd42f165c0d1796fd0ee74c90fd45
Mihael0599 <mihaeldraz10@gmail.com>
2025-06-28 15:24:22 +0200

    added header and hole chat section with slides menu on left and right

--- src/app/main-content/work-space-section/work-space-section.component.ts
+++ src/app/main-content/work-space-section/work-space-section.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDrawer, MatSidenavModule } from '@angular/material/sidenav';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-work-space-section',
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatSidenavModule,
+    MatSelectModule,
+    MatFormFieldModule
+  ],
   templateUrl: './work-space-section.component.html',
   styleUrl: './work-space-section.component.scss'
 })
 export class WorkSpaceSectionComponent {
+  isDrawerOpen = false;
 
+  toggleDrawer(drawer: MatDrawer) {
+    this.isDrawerOpen = !this.isDrawerOpen;
+    drawer.toggle();
+  }
 }
