67a544fa22e71c6df3642da384f98e92eb1bcf63
RobbyR98 <robbyrunge@yahoo.com>
2025-06-29 19:35:07 +0200

    refactor: update styles and component imports for better structure and maintainability

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 import { inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { LoginComponent } from "./login/login.component";
+import { ChatSectionComponent } from "./main-content/chat-section/chat-section.component";
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 interface Item {
   name: string;
@@ -10,7 +14,8 @@ interface Item {
 @Component({
   selector: 'app-root',
   imports: [
-    RouterOutlet
+    //LoginComponent,
+    ChatSectionComponent,
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
