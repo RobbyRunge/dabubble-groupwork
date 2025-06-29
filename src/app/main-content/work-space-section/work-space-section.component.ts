814aac80ad93ec132ade754a55238f8ab2237984
Mihael0599 <mihaeldraz10@gmail.com>
2025-06-29 15:38:12 +0200

    added chnalles dropwdonw menu and direct messages dropdown menu

--- src/app/main-content/work-space-section/work-space-section.component.ts
+++ src/app/main-content/work-space-section/work-space-section.component.ts
@@ -1,18 +1,18 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, viewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDrawer, MatSidenavModule } from '@angular/material/sidenav';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIcon } from '@angular/material/icon';
-import {
-  FormControl,
-  FormGroupDirective,
-  NgForm,
-  Validators,
-  FormsModule,
-  ReactiveFormsModule,
-} from '@angular/forms';
+import { MatAccordion, MatExpansionModule } from '@angular/material/expansion';
+import { MatInputModule } from '@angular/material/input';
+import { collection, collectionData, Firestore } from '@angular/fire/firestore';
+import { AsyncPipe } from '@angular/common';
 
+
+interface Item {
+  name: string;
+}
 @Component({
   selector: 'app-work-space-section',
   imports: [
@@ -21,7 +21,12 @@ import {
     MatSelectModule,
     MatFormFieldModule,
     MatIcon,
+    MatExpansionModule,
+    MatAccordion,
+    MatInputModule,
+    AsyncPipe
   ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './work-space-section.component.html',
   styleUrl: './work-space-section.component.scss'
 })
@@ -32,4 +37,10 @@ export class WorkSpaceSectionComponent {
     this.isDrawerOpen = !this.isDrawerOpen;
     drawer.toggle();
   }
+
+  accordion = viewChild.required(MatAccordion);
+
+  firestore = inject(Firestore);
+  itemCollection = collection(this.firestore, 'users');
+  item$ = collectionData(this.itemCollection);
 }
