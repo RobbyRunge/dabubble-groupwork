437beca36c7d6cb916e44341748a31319c3dec72
RobbyR98 <robbyrunge@yahoo.com>
2025-07-02 19:36:34 +0200

    feat: add routes for imprint and privacy policy pages and update footer links to use routerLink

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { AvatarComponent } from './signup/avatar/avatar.component';
 import { MainContentComponent } from './main-content/main-content.component';
+import { ImprintComponent } from './shared/imprint/imprint.component';
+import { PrivacyPolicyComponent } from './shared/privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
     { path: '', component: LoginComponent },
+    { path: 'imprint', component: ImprintComponent },
+    { path: 'privacy-policy', component: PrivacyPolicyComponent },
     { path: 'signup', component: SignupComponent },
     { path: 'avatar', component: AvatarComponent },
-    { path: 'mainpage', component: MainContentComponent }
+    { path: 'mainpage', component: MainContentComponent },
 ];
--- src/app/shared/footer-start/footer-start.component.ts
+++ src/app/shared/footer-start/footer-start.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer-start',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './footer-start.component.html',
   styleUrl: './footer-start.component.scss'
 })
