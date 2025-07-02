26d8f8ce96b25aa05a67fa3571f557e3228cc8fb
RobbyR98 <robbyrunge@yahoo.com>
2025-07-02 20:13:19 +0200

    feat: add password reset and email components with routing and styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,15 @@ import { AvatarComponent } from './signup/avatar/avatar.component';
 import { MainContentComponent } from './main-content/main-content.component';
 import { ImprintComponent } from './shared/imprint/imprint.component';
 import { PrivacyPolicyComponent } from './shared/privacy-policy/privacy-policy.component';
+import { PasswordSendEmailComponent } from './login/password-send-email/password-send-email.component';
+import { PasswordResetComponent } from './login/password-send-email/password-reset/password-reset.component';
 
 export const routes: Routes = [
     { path: '', component: LoginComponent },
     { path: 'imprint', component: ImprintComponent },
     { path: 'privacy-policy', component: PrivacyPolicyComponent },
+    { path: 'password-send-mail', component: PasswordSendEmailComponent },
+    { path: 'password-reset', component: PasswordResetComponent },
     { path: 'signup', component: SignupComponent },
     { path: 'avatar', component: AvatarComponent },
     { path: 'mainpage', component: MainContentComponent },
