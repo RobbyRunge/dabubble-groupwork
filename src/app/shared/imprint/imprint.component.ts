3dc5c22a7731346db27c8f0be3ca3f27539e7a3e
RobbyR98 <robbyrunge@yahoo.com>
2025-07-02 19:58:13 +0200

    feat: restructure imprint and privacy policy components with new layout and styling

--- src/app/shared/imprint/imprint.component.ts
+++ src/app/shared/imprint/imprint.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "../../main-content/header/header.component";
 import { HeaderStartComponent } from "../header-start/header-start.component";
-import { FooterStartComponent } from "../footer-start/footer-start.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-imprint',
-  imports: [HeaderStartComponent],
+  imports: [
+    HeaderStartComponent,
+    RouterLink,
+  ],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
--- src/app/shared/privacy-policy/privacy-policy.component.ts
+++ src/app/shared/privacy-policy/privacy-policy.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderStartComponent } from "../header-start/header-start.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-privacy-policy',
-  imports: [HeaderStartComponent],
+  imports: [
+    HeaderStartComponent,
+    RouterLink,
+  ],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
