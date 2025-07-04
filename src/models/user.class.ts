0e1c6b3f8695153d68bb2fe959efc1f8e8613230
RobbyR98 <robbyrunge@yahoo.com>
2025-07-04 22:21:12 +0200

    refactor(user.class): improve formatting and consistency in constructor and toJSON method

--- src/models/user.class.ts
+++ src/models/user.class.ts
@@ -6,33 +6,31 @@ export class User {
   aktiv!: boolean;
   avatar!: string;
   userId!: string;
-  chats : { data: { channel: string }; id: string }[] = [];
+  chats: { data: { channel: string }; id: string }[] = [];
   channels: { data: { channel: string }; id: string }[] = [];
 
-   constructor(obj?: any) {
-        
-        this.name = obj ? obj.name : '';
-        this.lastname = obj ? obj.lastname : '';
-        this.email = obj ? obj.email : '';
-        this.password = obj ? obj.password : '';
-        this.avatar = obj? obj.avatar : '';
-        this.userId! = obj ? obj.id : '';
-        this.chats = obj?.chats || [];
-        this.channels = obj?.channels || [];
-    }
+  constructor(obj?: any) {
 
-     public toJSON() {
-        return {
-            name: this.name,
-            lastname: this.lastname,
-            email: this.email,
-            password: this.password,
-            avatar: this.avatar,
-            userId: this.userId,
-            chats: this.chats,
-            channels: this.channels
-        }
+    this.name = obj ? obj.name : '';
+    this.lastname = obj ? obj.lastname : '';
+    this.email = obj ? obj.email : '';
+    this.password = obj ? obj.password : '';
+    this.avatar = obj ? obj.avatar : '';
+    this.userId! = obj ? obj.id : '';
+    this.chats = obj?.chats || [];
+    this.channels = obj?.channels || [];
+  }
+
+  public toJSON() {
+    return {
+      name: this.name,
+      lastname: this.lastname,
+      email: this.email,
+      password: this.password,
+      avatar: this.avatar,
+      userId: this.userId,
+      chats: this.chats,
+      channels: this.channels
     }
-  // mit avatar komponente erste erstellen?? Als leeres Array?
-  // avatarImg?: string;
+  }
 }
