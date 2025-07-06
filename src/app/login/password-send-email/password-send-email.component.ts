9e3a894cefa9c2431295fef7926df18a02928fed
RobbyR98 <robbyrunge@yahoo.com>
2025-07-06 14:04:44 +0200

    feat(password-send-email): implement email sent overlay and enhance button functionality

--- src/app/login/password-send-email/password-send-email.component.ts
+++ src/app/login/password-send-email/password-send-email.component.ts
@@ -1,17 +1,18 @@
 import { Component, inject } from '@angular/core';
 import { HeaderStartComponent } from '../../shared/header-start/header-start.component';
-import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 import { FooterStartComponent } from "../../shared/footer-start/footer-start.component";
 import { UserService } from '../../services/user.service';
 import { FormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-password-send-email',
   imports: [
     HeaderStartComponent,
-    RouterLink,
     FooterStartComponent,
-    FormsModule
+    FormsModule,
+    MatDialogModule,
   ],
   templateUrl: './password-send-email.component.html',
   styleUrl: './password-send-email.component.scss'
@@ -19,10 +20,24 @@ import { FormsModule } from '@angular/forms';
 export class PasswordSendEmailComponent {
   public userService = inject(UserService);
   public user = { email: '' };
+  private router = inject(Router)
 
   get isFormValid(): boolean {
     return (
       !!this.user.email
     );
+  }
+
+  async showSuccessfullyCreateContactOverlay() {
+    const backgroundOverlay = document.getElementById('background-overlay');
+    if (backgroundOverlay) {
+      backgroundOverlay.classList.add('active');
+      setTimeout(() => {
+        backgroundOverlay.classList.remove('active');
+        setTimeout(() => {
+          this.router.navigate(['/']);
+        }, 125);
+      }, 2000);
+    }
   }
 }
