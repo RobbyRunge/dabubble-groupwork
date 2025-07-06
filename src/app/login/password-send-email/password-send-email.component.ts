3af898788c48767b5a69562172b10a23e43d5dda
RobbyR98 <robbyrunge@yahoo.com>
2025-07-06 13:53:33 +0200

    refactor(password-send-email): enhance email input binding and form validation logic

--- src/app/login/password-send-email/password-send-email.component.ts
+++ src/app/login/password-send-email/password-send-email.component.ts
@@ -1,18 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { HeaderStartComponent } from '../../shared/header-start/header-start.component';
 import { RouterLink } from '@angular/router';
 import { FooterStartComponent } from "../../shared/footer-start/footer-start.component";
+import { UserService } from '../../services/user.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-password-send-email',
   imports: [
     HeaderStartComponent,
     RouterLink,
-    FooterStartComponent
-],
+    FooterStartComponent,
+    FormsModule
+  ],
   templateUrl: './password-send-email.component.html',
   styleUrl: './password-send-email.component.scss'
 })
 export class PasswordSendEmailComponent {
+  public userService = inject(UserService);
+  public user = { email: '' };
 
+  get isFormValid(): boolean {
+    return (
+      !!this.user.email
+    );
+  }
 }
