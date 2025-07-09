12cc08dcdf0f01a56978ead8a8476e170d425a61
Mihael0599 <mihaeldraz10@gmail.com>
2025-07-09 20:55:13 +0200

    added logout funktion

--- src/app/main-content/header/edit-logout-user/edit-logout-user.component.ts
+++ src/app/main-content/header/edit-logout-user/edit-logout-user.component.ts
@@ -1,9 +1,10 @@
 import { Component, Inject, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MAT_DIALOG_DATA, MatDialog, MatDialogActions } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogActions, MatDialogRef } from '@angular/material/dialog';
 import { UserCardComponent } from '../../work-space-section/user-card/user-card.component';
 import { User } from '../../../../models/user.class';
 import { UserService } from '../../../services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-logout-user',
@@ -14,9 +15,11 @@ import { UserService } from '../../../services/user.service';
 export class EditLogoutUserComponent {
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: { user: User, urlUserId: string },
-  ) { 
+    private dialogRef: MatDialogRef<EditLogoutUserComponent>,
+  ) {
     /* this.urlUserId = data.urlUserId; */
   }
+  private router = inject(Router);
   readonly dialog = inject(MatDialog);
   readonly userService = inject(UserService);
 
@@ -26,9 +29,14 @@ export class EditLogoutUserComponent {
 
     this.dialog.open(UserCardComponent, {
       data: {
-      user: currentUser,
-      urlUserId: currentUserId
-     }
+        user: currentUser,
+        urlUserId: currentUserId
+      }
     });
+  }
+
+  logout() {
+    this.router.navigate(['']);
+    this.dialogRef.close();
   }
 }
