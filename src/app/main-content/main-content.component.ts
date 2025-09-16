0127b787e5ec5b03b0866a6caf2bc3d955425f4d
Pradi8 <prathab8.4@googlemail.com>
2025-09-16 17:49:31 +0200

    responsive

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { ChatSectionComponent } from "./chat-section/chat-section.component";
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-main-content',
-  imports: [ChatSectionComponent, CommonModule],
+  imports: [ChatSectionComponent, CommonModule, HeaderComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
