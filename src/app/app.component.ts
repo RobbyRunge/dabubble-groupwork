cd51ca1b9f14c4e1328ddf7971d2cc9298637ec2
RobbyR98 <robbyrunge@yahoo.com>
2025-09-24 11:12:46 +0200

    feat: clean up app component by removing unused imports and code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,13 @@
 import { Component } from '@angular/core';
-import { inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Firestore, collectionData, collection } from '@angular/fire/firestore';
-import { LoginComponent } from "./login/login.component";
-import { ChatSectionComponent } from "./main-content/chat-section/chat-section.component";
-import {MatButtonModule} from '@angular/material/button';
-import {MatSidenavModule} from '@angular/material/sidenav';
-
-interface Item {
-  name: string;
-}
 
 @Component({
   selector: 'app-root',
   imports: [
-    //LoginComponent,
-    RouterOutlet,
-    //ChatSectionComponent,
-    
-],
+    RouterOutlet,    
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'DaBubble';
-
-  firestore = inject(Firestore);
-  itemCollection = collection(this.firestore, 'users');
-  item$ = collectionData(this.itemCollection);
 }
